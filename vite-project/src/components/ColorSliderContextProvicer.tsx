fea62e2afe9fee64301393efb46a22e69e73b02d
TadeasPikl <tadeas+1@piklovi.cz>
2024-01-22 15:56:13 +0100

    usereducer

--- vite-project/src/components/ColorSliderContextProvicer.tsx
+++ vite-project/src/components/ColorSliderContextProvicer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-type colorType = { R: number, G: number, B: number };
+type colorType = { R: number, G: number, B: number }
 
 type stateType = { 
     color: colorType
     changeColor: (color: colorType) => void
-};
+}
 
 const initialState: stateType = { 
     color: {R: 0, G: 0, B: 0 }, 
-    changeColor: (color: colorType) => { console.log(color)} 
-};
+    changeColor: (color: colorType) => { console.log(color); }
+}
 
 
 export const ColorSlidersContext = React.createContext(initialState);
@@ -20,18 +20,41 @@ interface IColorSliderProvider {
     children: React.ReactNode;
 }
 
+type ActionType = (
+    {type: "SET_COLOR", value: colorType } |
+    {type: "SET_COLOR_VALUE", value: number, colorValue: "R" | "G" | "B"}
+
+)
+
+function reducer(state: colorType, action: ActionType) {
+    switch (action.type) {
+        case "SET_COLOR":
+            state = action.value;
+            return state;
+        case "SET_COLOR_VALUE":
+            if (action.colorValue == "R") { state.R = action.value}
+            else if (action.colorValue == "B") { state.B = action.value}
+            else state.B = action.value
+            return state;
+        default:
+            return state;
+    }
+}
+
+
+
 export const ColorSliderContextProvider: React.FC<IColorSliderProvider> = ({ children }) => {
-    const [color, setColor] = React.useState<colorType>({R: 0, G: 0, B: 0 });
+    const [color, dispatch] = React.useReducer<React.Reducer<colorType, ActionType>>(reducer, {R: 0, G: 0, B: 0 });
 
     const changeColor = (color: colorType) => {
-        setColor(color);
-    };
+        dispatch({type: "SET_COLOR", value: color});
+    }
 
     return (
         <ColorSlidersContext.Provider value={{ color, changeColor }}>
             {children}
         </ColorSlidersContext.Provider>
-    );
+    )
 }
 
 export default ColorSliderContextProvider;
